985ab7f4df4d42c7742834215611cfc286008ad7
Ephriam Kassa <ephriam123@gmail.com>
2024-05-24 14:26:57 -0700

    third final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import '../../src/Header.css';
-import { gsap } from "gsap";
+import './Header.css';
 
 const Header = () => {
-
-    // gsap.to(".logo", { rotation: 90, x: 100, duration: 5 });
-
-
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src="./Pfp_hires 2.jpg" alt="Logo" className="logo-image" />
-                $BANIME
-            </div>
-            <nav>
-                <ul>
-                    <li>Chart</li>
-                    <li>Telegram</li>
-                    <li>Twitter</li>
-                    <li>Buy</li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./Pfp_hires 2.jpg" alt="Logo" className="logo-image" />
+        <h1 className="logo-text">BANIME</h1>
+      </div>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="/services" className="nav-link">Services</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
