f9a3da11876c903f72446fd4b09ba31ab159c459
Ephriam Kassa <ephriam123@gmail.com>
2024-05-25 19:08:44 -0700

    shuriken

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,11 @@ import "../../src/MainContent.css";
 import HorizontalImageLoop from "./HorizontalmageLoop";
 import ImageGallery from "./ImageGallery";
 import Tokenomics from "./Tokenomics";
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 const MainContent = () => {
   const scrollToHowToBuy = () => {
@@ -12,18 +16,48 @@ const MainContent = () => {
       .getElementById("how-to-buy")
       .scrollIntoView({ behavior: "smooth" });
   };
+  const shurikenRef = useRef();
+
+  useGSAP(() => {
+    const boxes = gsap.utils.toArray(shurikenRef.current.children);
+    boxes.forEach((box) => {
+      gsap.fromTo(
+        box,
+        {
+          x: "-100vw",
+          rotation: -360,
+          duration: 3,
+          scrollTrigger: {
+            trigger: box,
+            start: "center, center",
+            scrub: true,
+          }
+        },
+        {
+          x: "100vw",
+          rotation: 360,
+          duration: 3,
+          scrollTrigger: {
+            trigger: box,
+            start: "center, center",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, []);
 
   return (
     <div>
-      <main className="main-content">
-      </main>
-   
+      <main className="main-content"></main>
       <ImageGallery />
-      <div className="filler"></div>
-      <Tokenomics />
-\      
-
-      {/* <TimelineNew /> */}
+      <div className="filler">
+        <div ref={shurikenRef} className="shurikenBox">
+          <img src="./shuriken.png" className="shuriken" alt="shuriken" />
+          <img src="./shuriken.png" className="shuriken" alt="shuriken" />
+        </div>
+      </div>
+      <Tokenomics />\{/* <TimelineNew /> */}
       {/* <iframe 
           src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1TL0Nj2dAGFl33J-VPOCaSr5QZ5l6gMF1EKalmPyhMRA&font=Default&lang=en&initial_zoom=2&height=650'
           width='50%' 
